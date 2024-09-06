c13a559c132d0a99e2879dc78ffed4aaa19e6a9d
Renato Pablo <renatocuim@gmail.com>
2024-09-06 13:50:46 -0300

    correcao

--- pages/cadastrar-cliente.php
+++ pages/cadastrar-cliente.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="../css/style.css">
     <link href="../fontawesome/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/padrao-cadastro.css">
-    <link rel="stylesheet" href="../css/popup-not.css">~
+    <link rel="stylesheet" href="../css/popup-not.css">
     
     <title>Cadastro Cliente</title>
 </head>
@@ -68,135 +68,139 @@
         <!-- <img class="imgCadastroPessoa" src="../image/perfilCadastro.png" alt="foto cadastro pessoa"> -->
         
         <form action="../config/processa_dados.php" method="post">
-            <div class="container-cadastro">
-            <h2>Dados Pessoais</h2>
-            <br>
-                <!-- select para indeficar se pessoa fisica ou juridica -->
-                 
-                <label class="radio-button">
-                    <input type="radio" name="example-radio" value="fisica">
-                    <span class="radio"></span>
-                    Pessoa Física.
-                </label>
+            
+        
 
-                <label class="radio-button">
-                    <input type="radio" name="example-radio" value="juridica">
-                    <span class="radio"></span>
-                    Pessoa Jurídica
-                </label>
-
-
-
-                <div class="input-container">
-                    <label for="nome">Nome Completo:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-                 <div class="input-container">
-                    <div class="ls-custom-select">
-                        <label for="genero">Gênero:</label>
-                        <select id="genero" name="genero" class="escolha-genero" aria-label="selecione">
-                            <option selected="">Selecione..</option>
-                            <img src="../image/seta.png" alt="Logo CarCheck" title="CarCheck">
-                            <option value="1">Masculino</option>
-                            <option value="2">Feminino</option>
-                            <option value="3">Outro</option>
-                        </select>
+                <div class="cadastrar-cliente">
+                    <div class="dados-pessoais">
+                        <h2>Dados Pessoais</h2>
+                        <br>
+                        <!-- select para indeficar se pessoa fisica ou juridica -->
+                    
+                        <label class="radio-button">
+                            <input type="radio" name="example-radio" value="fisica">
+                            <span class="radio"></span>
+                            Pessoa Física.
+                        </label>
+                        <label class="radio-button">
+                            <input type="radio" name="example-radio" value="juridica">
+                            <span class="radio"></span>
+                            Pessoa Jurídica
+                        </label>
+                        <div class="input-container">
+                            <label for="nome">Nome Completo:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <div class="ls-custom-select">
+                                <label for="genero">Gênero:</label>
+                                <select id="genero" name="genero" class="escolha-genero" aria-label="selecione">
+                                    <option selected="">Selecione..</option>
+                                    <!-- <img src="../image/seta.png" alt="Logo CarCheck" title="CarCheck"> -->
+                                    <option value="1">Masculino</option>
+                                    <option value="2">Feminino</option>
+                                    <option value="3">Outro</option>
+                                </select>
+                            </div>
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="telefone">Telefone:</label>
+                            <input id="telefone" name="telefone" placeholder="Digite seu Telefone" type="number" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="email">Email:</label>
+                            <input id="email" name="email" placeholder="Digite seu email" type="email" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="datadenascimento">Data de Nascimento:</label>
+                            <input id="datadenascimento" name="datadenascimento" placeholder="Digite sua Data de Nascimento" type="date" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="senha">Senha:</label>
+                            <input id="senha" name="senha" placeholder="Insira uma Senha" type="password" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="confirmarsenha">Confirmar Senha:</label>
+                            <input id="confirmarsenha" name="confirmarsenha"placeholder="Confirme sua senha" type="password" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <!-- campos especificos pessoa fisica/juridica -->
+                        <div id="campos-fisica" class="especifico">
+                            <div class="input-container">
+                                <label for="cpf">CPF:</label>
+                                <input id="cpf" name="cpf" maxlength="14" placeholder="000.000.000-00" oninput="mascaraCPF(this)" type="text" class="input">
+                            </div>
+                                <div class="underline"></div>
+                            <div class="input-container">
+                                <label for="cpf">RG:</label>
+                                <input id="rg" name="rg" maxlength="13" placeholder="00.000.000-00" oninput="mascaraRG(this)" type="text" class="input">
+                            <div class="underline"></div>
+                            </div>
+                        </div>
+                        <div id="campos-juridica" class="especifico">
+                            <div class="input-container">
+                                <label for="cnpj">CNPJ:</label>
+                                <input id="cnpj" name="cnpj" maxlength="18" placeholder="00.000.000/0000-00" oninput="mascaraCNPJ(this)" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <!-- <div class="input-container">
+                            <label for="cpf">CPF:</label>
+                            <input id="cpf" name="cpf" placeholder="Digite seu CPF" type="number" class="input">
+                            <div class="underline"></div>
+                        </div> -->
                     </div>
-                    <div class="underline"></div>
+                    <br>
+                    <h2>Endereço</h2>
+                    <div class="endereco">
+                        <div class="input-container">
+                            <label for="senha">Estado:</label>
+                            <input id="senha" name="senha" placeholder="Insira uma Senha" type="password" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="nome">Rua:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="nome">Cidade:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="nome">Numero:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="nome">Bairro:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="nome">Complemento:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="nome">CEP:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                        <div class="input-container">
+                            <label for="nome">Ponto de Referência:</label>
+                            <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
+                            <div class="underline"></div>
+                        </div>
+                    </div>
                 </div>
-                 <div class="input-container">
-                    <label for="telefone">Telefone:</label>
-                    <input id="telefone" name="telefone" placeholder="Digite seu Telefone" type="number" class="input">
-                    <div class="underline"></div>
-                </div>
-                 <div class="input-container">
-                    <label for="email">Email:</label>
-                    <input id="email" name="email" placeholder="Digite seu email" type="email" class="input">
-                    <div class="underline"></div>
-                </div>
-                 <div class="input-container">
-                    <label for="datadenascimento">Data de Nascimento:</label>
-                    <input id="datadenascimento" name="datadenascimento" placeholder="Digite sua Data de Nascimento" type="date" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="senha">Senha:</label>
-                    <input id="senha" name="senha" placeholder="Insira uma Senha" type="password" class="input">
-                    <div class="underline"></div>
-                </div>
-
-                <div id="campos-fisica" class="especifico">
-                    <div class="input-container">
-                        <label for="cpf">CPF:</label>
-                        <input id="cpf" name="cpf" maxlength="14" placeholder="000.000.000-00" oninput="mascaraCPF(this)" type="text" class="input">
-                    <div class="underline"></div>
-                    <div class="input-container">
-                        <label for="cpf">RG:</label>
-                        <input id="rg" name="rg" maxlength="13" placeholder="00.000.000-00" oninput="mascaraRG(this)" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-
-                <div id="campos-juridica" class="especifico">
-                    <div class="input-container">
-                        <label for="cnpj">CNPJ:</label>
-                        <input id="cnpj" name="cnpj" maxlength="18" placeholder="00.000.000/0000-00" oninput="mascaraCNPJ(this)" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-
-                <!-- <div class="input-container">
-                    <label for="cpf">CPF:</label>
-                    <input id="cpf" name="cpf" placeholder="Digite seu CPF" type="number" class="input">
-                    <div class="underline"></div>
-                </div> -->
-                <div class="input-container">
-                    <label for="confirmarsenha">Confirmar Senha:</label>
-                    <input id="confirmarsenha" name="confirmarsenha"placeholder="Confirme sua senha" type="password" class="input">
-                    <div class="underline"></div>
-                </div>
-                <br>
-        <h2>Endereço</h2>
-                <div class="input-container">
-                    <label for="senha">Estado:</label>
-                    <input id="senha" name="senha" placeholder="Insira uma Senha" type="password" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="nome">Rua:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="nome">Cidade:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="nome">Numero:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="nome">Bairro:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="nome">Complemento:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="nome">CEP:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-                <div class="input-container">
-                    <label for="nome">Ponto de Referência:</label>
-                    <input id="nome" name="nome" placeholder="Digite seu Nome" type="text" class="input">
-                    <div class="underline"></div>
-                </div>
-            </div>
+        
             <button class="botao-submit" type="submit">ENVIAR</button>
         </form>
     
