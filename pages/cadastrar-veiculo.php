7a771e4462eb81dfe5bd18c60d83ea0af6b99f46
eugbm <gabi_batistamatos@hotmail.com>
2024-10-02 22:23:01 -0300

    fazendo tela de cadastrar veiculo

--- pages/cadastrar-veiculo.php
+++ pages/cadastrar-veiculo.php
@@ -1,10 +1,8 @@
 <?php
     session_start();
-    // print_r($_SESSION);
     if(!isset($_SESSION) OR $_SESSION['logado'] != true):
-		header("location: ../config/sair.php");		
-	else:
-
+        header("location: ../config/sair.php");
+    else:
         $permissao = $_SESSION['permissaoUsuario'];
 ?>
 <!DOCTYPE html>
@@ -17,13 +15,10 @@
     <link href="../fontawesome/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/popup-not.css">
     <link rel="stylesheet" href="../css/cadastrar-veiculo.css">
-    <style>
-    </style>
 </head>
 <body>
-    <header>
 
-        <!-- Sidebar -->
+    <header>
         <div class="sidebar">
             <a href="../pages/home-funci.php">Home</a>
             <a href="../pages/cadastrar-funci.php">Cadastrar Funcionário</a>
@@ -32,28 +27,22 @@
         </div>
 
         <div class="container-header">
-            
             <img src="../image/logo-carcheck.png" alt="Logo CarCheck" title="CarCheck">
             <h1>CarCheck</h1>
-            
-            
         </div>
 
         <div class="icons">
-            <!-- Botão para mostrar a notificação -->
             <button onclick="showNotification()" class="icons-not">
-            <i class="fa-solid fa-bell fa-2xl" style="color: #ffffff;"></i></button>
+                <i class="fa-solid fa-bell fa-2xl" style="color: #ffffff;"></i>
+            </button>
 
-            <!-- Popup de notificação -->
             <div id="notification" class="notification">
                 <span id="notification-text">Este é um alerta de notificação!</span>
                 <span class="close-btn" onclick="closeNotification()">&times;</span>
             </div>
 
-            <!-- <a href="pages/notificacao.html"><i class="fa-solid fa-bell fa-2xl" style="color: #ffffff;"></i></a> -->
-
             <?php if($permissao == 2 || $permissao == 3) : ?>
-            <a href="../pages/home-funci.php"><i class="fa-solid fa-house-chimney fa-2xl casa" style="color: #ffffff;"></i></a>
+                <a href="../pages/home-funci.php"><i class="fa-solid fa-house-chimney fa-2xl casa" style="color: #ffffff;"></i></a>
             <?php endif; ?>
 
             <?php if($permissao == 1) : ?>
@@ -62,47 +51,48 @@
             
             <a href="../pages/perfil.php"><i class="fa-solid fa-user fa-2xl" style="color: #ffffff;"></i></a>
         </div>
+
         <input type="checkbox" id="checkbox" onclick="toggleSidebar()">
         <label for="checkbox" class="toggle">
             <div class="bar bar--top"></div>
             <div class="bar bar--middle"></div>
             <div class="bar bar--bottom"></div>
         </label>
-
-
     </header>
 
+    <main>
+        <h2 class="titulo-formulario">Cadastrar Veículo</h2>
         <form action="../config/processa-cadastro-veiculo.php" method="post">
-            <div class="cadastro-veiculo">
-                <div>
-                    <label for="prop">Proprietário</label>
-                    <input id="prop" type="text" name="proprietario" class="input" onkeyup="buscarProprietarios()" autocomplete="off">
-                    <ul id="sugestoes" class="suggestions"></ul>
-                </div>
-                <div>
-                    <label for="placa">Placa</label>
-                    <input id="placa" type="text" name="placa" class="input" oninput="mascaraPlacaVeiculo(this)">
-                </div>
-                <div>
-                    <label for="cor">Cor</label>
-                    <input id="cor" name="cor" type="text" class="input">
-                </div>
-                <div>
-                    <label for="tipo">Tipo Veículo</label>
-                    <input id="tipo" name="tipo" type="text" class="input">
-                </div>
-                <div>
-                    <label for="modelo">Modelo</label>
-                    <input id="modelo" name="modelo" type="text" class="input">
-                </div>
-                <div>
-                    <label for="marca">Marca</label>
-                    <input id="marca" name="marca" type="text" class="input">
-                </div>
-                <button type="submit">Enviar</button>
-            </div>
 
+            <div class="prop">
+                <label for="prop">Proprietário</label>
+                <input id="prop" type="text" name="proprietario" class="input" onkeyup="buscarProprietarios()" autocomplete="off">
+                <ul id="sugestoes" class="suggestions"></ul>
+            </div>
+            <div>
+                <label for="placa">Placa</label>
+                <input id="placa" type="text" name="placa" class="input" oninput="mascaraPlacaVeiculo(this)">
+            </div>
+            
+            <div>
+                <label for="modelo">Modelo</label>
+                <input id="modelo" name="modelo" type="text" class="input">
+            </div>
+            <div>
+                <label for="tipo">Tipo Veículo</label>
+                <input id="tipo" name="tipo" type="text" class="input">
+            </div>
+            <div>
+                <label for="cor">Cor</label>
+                <input id="cor" name="cor" type="text" class="input">
+            </div>
+            <div>
+                <label for="marca">Marca</label>
+                <input id="marca" name="marca" type="text" class="input">
+            </div>
+            <button type="submit">Enviar</button>
         </form>
+    </main>
 
     <script src="../js/script.js"></script>
     <script src="../js/popup-not.js"></script>
