a53094d6254ad2b4f21446a55b7b2ab253b9462c
eugbm <gabi_batistamatos@hotmail.com>
2024-10-22 21:34:03 -0300

    estilo

--- pages/forma-pagamento.php
+++ pages/forma-pagamento.php
@@ -17,7 +17,6 @@
     <link rel="stylesheet" href="../css/forma-pagamento.css">
     <link href="../fontawesome/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-<<<<<<< Updated upstream
     <title>Gerenciamento de Itens</title>
 
     <style>
